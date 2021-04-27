e4c4c727ef8565b84d321c458fa5b62e5029c746
Mufassir-Chowdhury <mac22214u@gmail.com>
2021-04-28 04:51:26 +0600

    bubble sort

--- CSE-137 Data Structures/Sorting/Bubble Sorting.cpp
+++ CSE-137 Data Structures/Sorting/Bubble Sorting.cpp
@@ -2,15 +2,34 @@
 #include<vector>
 
 int main(){
+    std::cout << " ---------------------------- " << std::endl;
+    std::cout << " \"Bubble Sorting\" program started" << std::endl;
+    std::cout << " Written by \"Mufassir Ahmad Chowdhury\"" << std::endl << std::endl;
     int n;
+    std::cout << "Enter the size of the array:" << std::endl;
     std::cin >> n;
 
     std::vector<int> numbers(n);
     
+    std::cout << "Enter the values of the array:" << std::endl;
     for(auto &i: numbers)
         std::cin >> i;
 
+    std::cout << "Sorting started..." << std::endl;
     
+    for(size_t i = 0; i < numbers.size(); i++)
+        for(size_t j = i+1; j<numbers.size(); j++)
+            if(numbers[j] < numbers[i])
+                std::swap(numbers[j], numbers[i]);
+
+    std::cout << "Sorting finished!" << std::endl;
+    
+    std::cout << std::endl << "Values in the array after sorting is: " << std::endl;
+    for(auto i: numbers)
+        std::cout << i << " ";
+    
+    std::cout << std::endl;
+    std::cout << " ---------------------------- " << std::endl;
 
     return 0;
 }
